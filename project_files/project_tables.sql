41bb5d9294d90cd851a7a40a8b0006bc708b7ae5
Nathan Ortolan <ndo28@humboldt.edu>
2016-10-09 22:32:45 -0700

    Nathan's updates to project_tables.sql - Added Users, Admins, Surveyors, Reporters tables

--- project_files/project_tables.sql
+++ project_files/project_tables.sql
@@ -7,6 +7,63 @@
 
 spool project-design-out.txt
 
+
+--============================================================
+-- Users Table
+-- Determined by a user name (admin or surveyor)
+-- user_name : admin if Administrator login, surveyor if Surveyor login
+
+drop table Users cascade constraints;
+
+create table Users
+(
+	user_name			                    varchar2 check(user_name in ('admin', 'surveyor')) not null,
+	password                          varchar2 not null,
+	primary key (menu_id)
+);
+
+
+--============================================================
+-- Admins
+-- Determined by a 2-digit admin id
+-- admin_id : 2-digit id determined by sequence admin_seq
+
+drop table Admins cascade constraints;
+
+create table Admins
+(
+	admin_id			                   integer(2) not null,
+  admin_lname                      varchar2 not null,
+  admin_fname                      varchar2 not null,
+  admin_email                      varchar2 not null,
+  --admin_phone                    --integer(10)???,
+  user_name                        varchar2,
+	primary key (admin_id),
+  foreign key (user_name) references Users
+);
+
+
+--============================================================
+-- Surveyors
+-- Determined by a 3-digit surveyor id
+-- surv_id : 3-digit id determined by sequene surveyor_seq
+
+drop table Surveyors cascade constraints;
+
+create table Surveyors
+(
+	surv_id			                   integer(3) not null,
+  surv_lname                     varchar2 not null,
+  surv_fname                     varchar2 not null,
+  surv_email                     varchar2 not null,
+  --surv_phone                   --integer(10)???,
+  user_name                      varchar2,
+	primary key (surv_id),
+  foreign key (user_name) references Users
+);
+
+
+
 --============================================================
 --Reports table => purpose: to track important information about
 --     beach walks.
@@ -21,9 +78,9 @@ create table Reports
  end_time                       time not null,
  beach_abbr                     char(4),
  survey_summary                 long varchar2,
-  primary key (report_id),
-  foreign key (beach_abbr) references Beaches
- );
+ primary key (report_id),
+ foreign key (beach_abbr) references Beaches
+);
 
 
 --============================================================
@@ -33,11 +90,11 @@ create table Reports
 --                      are Y/N fields
 
 
-Drop table Report_entries cascade constraints;
+drop table Report_entries cascade constraints;
 
 create table Report_entries
 (PRN				                    varchar(30) not null,
- surveyor_id                    integer(3),
+ surv_id                        integer(3),
  report_id                      integer(5),
  species_abbr			              char(4),
  LAT                            decimal(7,2),
@@ -50,15 +107,48 @@ create table Report_entries
  no_of_animals                  integer(2),
  primary key (PRN),
  foreign key (species_abbr) references Species,
- foreign key (surveyor_id) references Surveyor,
+ foreign key (surv_id) references Surveyors,
  foreign key (report_id) references Reports
 );
 
 
+--============================================================
+-- Reporters
+-- Surveyors act as Reporters while completing reports
+-- Determined by a 3-digit surveyor id and a 5-digit report_id
+-- surveyor email and phone will be selected from the Surveyor table by reference
+
+drop table Reporters cascade constraints;
+
+create table Reporters
+(
+  surv_id                       integer(3),
+  report_id                     integer(5),
+	primary key (surv_id, report_id),
+  foreign key (surv_id) references Surveyors,
+  foreign key (report_id) references Reports
+);
+
+
+
+
+--============================================================
+-- Sequences
+
+-- admin_seq
+-- Sequence for admin ids
+drop sequence admin_seq;
+create sequence admin_seq
+start with 1;
+
+-- surveyor_seq
+-- Sequence for surveyor ids
 drop sequence surveyor_seq;
 create sequence surveyor_seq
 start with 1;
 
+-- report_seq
+-- Sequence for report ids
 drop sequence report_seq;
 create sequence report_seq
 start with 1000;
