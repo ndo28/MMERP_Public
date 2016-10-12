2857b75bd5f98bdb25fcdd3ec99d8ccf947e5885
Guthrie Hayward <gmh234@humboldt.edu>
2016-10-12 09:04:37 -0700

    Added beach and species tables for real this time

--- project_files/project_tables.sql
+++ project_files/project_tables.sql
@@ -3,66 +3,9 @@
 --Guthrie Hayward
 --Abdul Shaikh
 -- CS 458 - Fall 2016
--- last modified: 2016-10-08
+-- last modified: 2016-10-09
 
 spool project-design-out.txt
-
-
---============================================================
--- Users Table
--- Determined by a user name (admin or surveyor)
--- user_name : admin if Administrator login, surveyor if Surveyor login
-
-drop table Users cascade constraints;
-
-create table Users
-(
-	user_name			                    varchar2 check(user_name in ('admin', 'surveyor')) not null,
-	password                          varchar2 not null,
-	primary key (menu_id)
-);
-
-
---============================================================
--- Admins
--- Determined by a 2-digit admin id
--- admin_id : 2-digit id determined by sequence admin_seq
-
-drop table Admins cascade constraints;
-
-create table Admins
-(
-	admin_id			                   integer(2) not null,
-  admin_lname                      varchar2 not null,
-  admin_fname                      varchar2 not null,
-  admin_email                      varchar2 not null,
-  --admin_phone                    --integer(10)???,
-  user_name                        varchar2,
-	primary key (admin_id),
-  foreign key (user_name) references Users
-);
-
-
---============================================================
--- Surveyors
--- Determined by a 3-digit surveyor id
--- surv_id : 3-digit id determined by sequene surveyor_seq
-
-drop table Surveyors cascade constraints;
-
-create table Surveyors
-(
-	surv_id			                   integer(3) not null,
-  surv_lname                     varchar2 not null,
-  surv_fname                     varchar2 not null,
-  surv_email                     varchar2 not null,
-  --surv_phone                   --integer(10)???,
-  user_name                      varchar2,
-	primary key (surv_id),
-  foreign key (user_name) references Users
-);
-
-
 
 --============================================================
 --Reports table => purpose: to track important information about
@@ -78,9 +21,9 @@ create table Reports
  end_time                       time not null,
  beach_abbr                     char(4),
  survey_summary                 long varchar2,
- primary key (report_id),
- foreign key (beach_abbr) references Beaches
-);
+  primary key (report_id),
+  foreign key (beach_abbr) references Beaches
+ );
 
 
 --============================================================
@@ -90,11 +33,11 @@ create table Reports
 --                      are Y/N fields
 
 
-drop table Report_entries cascade constraints;
+Drop table Report_entries cascade constraints;
 
 create table Report_entries
 (PRN				                    varchar(30) not null,
- surv_id                        integer(3),
+ surveyor_id                    integer(3),
  report_id                      integer(5),
  species_abbr			              char(4),
  LAT                            decimal(7,2),
@@ -107,50 +50,41 @@ create table Report_entries
  no_of_animals                  integer(2),
  primary key (PRN),
  foreign key (species_abbr) references Species,
- foreign key (surv_id) references Surveyors,
+ foreign key (surveyor_id) references Surveyor,
  foreign key (report_id) references Reports
 );
 
 
---============================================================
--- Reporters
--- Surveyors act as Reporters while completing reports
--- Determined by a 3-digit surveyor id and a 5-digit report_id
--- surveyor email and phone will be selected from the Surveyor table by reference
-
-drop table Reporters cascade constraints;
-
-create table Reporters
-(
-  surv_id                       integer(3),
-  report_id                     integer(5),
-	primary key (surv_id, report_id),
-  foreign key (surv_id) references Surveyors,
-  foreign key (report_id) references Reports
-);
-
-
-
-
---============================================================
--- Sequences
-
--- admin_seq
--- Sequence for admin ids
-drop sequence admin_seq;
-create sequence admin_seq
-start with 1;
-
--- surveyor_seq
--- Sequence for surveyor ids
 drop sequence surveyor_seq;
 create sequence surveyor_seq
 start with 1;
 
--- report_seq
--- Sequence for report ids
 drop sequence report_seq;
 create sequence report_seq
 start with 1000;
+
+/*
+  Table name: beach
+  Table contents: names and abbreviations of
+  beaches covered by MMERP.
+*/
+drop table beach cascade constraints;
+create table beach
+  (
+    BEACH_ABBR varchar(2), --Beach abbreviation
+    beach_name varchar(45) -- Beach name or description (Based on the data sheet document)
+  );
+
+/*
+  Table name: species
+  Table contents: names and abbreviations of
+  species covered by MMERP.
+*/
+drop table species cascade constraints;
+create table species
+  (
+    SPEC_ABBR varchar(4), --Species abbreviation
+    spec_name varchar(30) -- Species name or description (Based on the data sheet document)
+  );
 
 spool off
