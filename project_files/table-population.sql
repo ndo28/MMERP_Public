1f5b31cc7b2c5ba34d859608e93b015527348258
Guthrie Hayward <gmh234@humboldt.edu>
2016-10-16 15:35:56 -0700

    population of Beaches table

---
+++ project_files/table-population.sql
@@ -0,0 +1,54 @@
+--Rebecca Williams
+--Nathan Ortolan
+--Guthrie Hayward
+--Abdul Shaikh
+-- CS 458 - Fall 2016
+-- Last Edited 10/16/16
+
+/*
+Populating Beaches table
+*/
+insert into Beaches
+   values ('CV','Centerville')
+insert into Beaches
+    values('TB','Table Bluff')
+insert into Beaches
+    values('SS','South Spit')
+insert into Beaches
+    values('BA','Bay Street to Powerline')
+insert into Beaches
+    values('PL','Powerline to Manila Community Center')
+insert into Beaches
+    values('MN','Manila Community Center to Ma-lel Dunes')
+insert into Beaches
+    values('MA','Ma-lel Dunes to Mad River Parking Lot')
+insert into Beaches
+    values('MR','Mad River Parking Lot to Mouth of Mad River')
+insert into Beaches
+    values('CL','Clam Beach')
+insert into Beaches
+    values('MO','Moonstone')
+insert into Beaches
+    values('HP','Houda Point')
+insert into Beaches
+    values('LU','Luffenholtz')
+insert into Beaches
+    values('BK','Baker')
+insert into Beaches
+    values('IB','Indian')
+insert into Beaches
+    values('TR','Trinidad')
+insert into Beaches
+    values('CO','College Cove')
+insert into Beaches
+    values('AG','Agate')
+insert into Beaches
+    values('BL','Big Lagoon')
+insert into Beaches
+    values('DL','Dry Lagoon')
+insert into Beaches
+    values('SL','Stone Lagoon')
+insert into Beaches
+    values('CC','Crescent City Beach')
+insert into Beaches
+    values('PB','Pelican State Beach')
