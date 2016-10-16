5cfdbb0c82569dfac4dd0b58d15edd4db4aca6a9
Guthrie Hayward <gmh234@humboldt.edu>
2016-10-16 15:39:59 -0700

    Added semicolons. lol.

--- project_files/table-population.sql
+++ project_files/table-population.sql
@@ -5,50 +5,56 @@
 -- CS 458 - Fall 2016
 -- Last Edited 10/16/16
 
+spool table-population-status.txt
+
 /*
 Populating Beaches table
 */
 insert into Beaches
-   values ('CV','Centerville')
+   values ('CV','Centerville');
 insert into Beaches
-    values('TB','Table Bluff')
+    values('TB','Table Bluff');
 insert into Beaches
-    values('SS','South Spit')
+    values('SS','South Spit');
 insert into Beaches
-    values('BA','Bay Street to Powerline')
+    values('BA','Bay Street to Powerline');
 insert into Beaches
-    values('PL','Powerline to Manila Community Center')
+    values('PL','Powerline to Manila Community Center');
 insert into Beaches
-    values('MN','Manila Community Center to Ma-lel Dunes')
+    values('MN','Manila Community Center to Ma-lel Dunes');
 insert into Beaches
-    values('MA','Ma-lel Dunes to Mad River Parking Lot')
+    values('MA','Ma-lel Dunes to Mad River Parking Lot');
 insert into Beaches
-    values('MR','Mad River Parking Lot to Mouth of Mad River')
+    values('MR','Mad River Parking Lot to Mouth of Mad River');
 insert into Beaches
-    values('CL','Clam Beach')
+    values('CL','Clam Beach');
 insert into Beaches
-    values('MO','Moonstone')
+    values('MO','Moonstone');
 insert into Beaches
-    values('HP','Houda Point')
+    values('HP','Houda Point');
 insert into Beaches
-    values('LU','Luffenholtz')
+    values('LU','Luffenholtz');
 insert into Beaches
-    values('BK','Baker')
+    values('BK','Baker');
 insert into Beaches
-    values('IB','Indian')
+    values('IB','Indian');
 insert into Beaches
-    values('TR','Trinidad')
+    values('TR','Trinidad');
 insert into Beaches
-    values('CO','College Cove')
+    values('CO','College Cove');
 insert into Beaches
-    values('AG','Agate')
+    values('AG','Agate');
 insert into Beaches
-    values('BL','Big Lagoon')
+    values('BL','Big Lagoon');
 insert into Beaches
-    values('DL','Dry Lagoon')
+    values('DL','Dry Lagoon');
 insert into Beaches
-    values('SL','Stone Lagoon')
+    values('SL','Stone Lagoon');
 insert into Beaches
-    values('CC','Crescent City Beach')
+    values('CC','Crescent City Beach');
 insert into Beaches
-    values('PB','Pelican State Beach')
+    values('PB','Pelican State Beach');
+
+commit;
+
+spool off
