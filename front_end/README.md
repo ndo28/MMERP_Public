253f40d547e2f5c2b8cb6c4faf7f56c15ba3afce
Abdul Shaikh <abdulamite@gmail.com>
2016-11-30 15:44:50 -0800

    fixed files outside frontend

---
+++ front_end/README.md
@@ -0,0 +1,2 @@
+# The_Bang_Show
+Team Repo for The_Bang_Show
---
+++ front_end/admin_main_menu.php
@@ -0,0 +1,50 @@
+<?php
+/*--------
+main_menu.php
+
+Guthrie Hayward (gmh234)
+Nathan Ortolan (ndo28)
+Becky Williams (rjw125)
+Abdul Shaikh (ats234)
+
+Created by Abdul, Guthrie and Rebecca on 11/5/16
+
+Modified by: rjw, ats  on: 11/13/16
+Modified by: rjw  on: 11/20/16
+
+    function: main_menu
+    purpose: a form containing four submit buttons with the following
+    choices:
+        --option to go to Admin console
+        --option to go to list existing reports for user
+        --option to go to create a new report
+        --option to change password
+
+-------*/
+
+
+function admin_main_menu()
+{
+
+        ?>
+        <div class="login-block">
+          <form method="post"
+                action="<?= htmlentities($_SERVER['PHP_SELF'],
+                                         ENT_QUOTES) ?>">
+            <fieldset>
+              <h1> Welcome to MMERP! </h1>
+              <h2> Would you like to create a new report, or continue an exisiting report? </h2>
+
+              <div class="chooseItem">
+                  <input type="submit" name="admin" value="Admin Console"/>
+                  <input type="submit" name="new" value="New Report"/>
+                  <input type="submit" name="continue" value="Continue"/>
+
+              </div>
+            </fieldset>
+          </form>
+        </div>
+        <?php
+        //<input type="submit" name="change_password" value="Change Password" />
+        }
+?>
